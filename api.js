dd96817b4cd02fca6994291845657bc4f55f2ebc
pvegesna1 <pavan.vegesna@slu.edu>
2026-04-12 23:00:59 -0500

    api.js

--- api.js
+++ api.js
@@ -1,1 +1,37 @@
+// api.js
+const path = require('path')
+
+ /**
+ * Handle the root route
+ * @param {object} req
+ * @param {object} res
+*/
+function handleRoot (req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+}
+
+/**
+ * List all products
+ * @param {object} req
+ * @param {object} res
+ */
+async function listProducts (req, res) {
+  // Add CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  
+  // Read the products file
+  const productsFile = path.join(__dirname, 'data/full-products.json')
+  
+  try {
+    const data = await fs.readFile(productsFile)
+    res.json(JSON.parse(data))
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+}
+
+module.exports = {
+  handleRoot,
+  listProducts
+}
 
